d1180ada684d21c662290db15d022a034fd3706c
Nikita Reshetnikov <60236568+Mortiferus@users.noreply.github.com>
2021-03-14 13:36:23 +0300

    Update PageUiTest.java

--- src/test/java/PageUiTest.java
+++ src/test/java/PageUiTest.java
@@ -40,7 +40,7 @@ public class PageUiTest {
         $("[data-test-id=city] .input__control").setValue("Мо");
         $$(".menu-item__control").findBy(text("Москва")).click();
         $("[data-test-id=date] [placeholder=\"Дата встречи\"]").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE, whenCalendar(false), Keys.CONTROL + "A", Keys.DELETE);
-        $$(".calendar__day").findBy(text(when(true))).click();
+        $$(".calendar__day").findBy(text(whenCalendar(true))).click();
         $("[data-test-id=name] [name=name]").setValue("Альберт Эйнштейн");
         $("[data-test-id=phone] [name=phone]").setValue("+14318791955");
         $("[data-test-id=agreement]>.checkbox__box").click();
